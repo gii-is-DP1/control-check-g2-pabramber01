f8df13d84bd1935927ee53b7cf93a8a5719f643a
unknown <pramosb01@gmail.com>
2022-01-14 09:10:59 +0100

    Ejercicio 7 hecho

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingTypeFormatter.java
@@ -3,22 +3,28 @@ package org.springframework.samples.petclinic.feeding;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FeedingTypeFormatter implements Formatter<FeedingType>{
 
+	@Autowired
+	private FeedingService feedingService;
+	
     @Override
     public String print(FeedingType object, Locale locale) {
-        // TODO Auto-generated method stub
-        return null;
+        return object.getName();
     }
 
     @Override
     public FeedingType parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+        FeedingType ft = this.feedingService.getFeedingType(text);
+        if (ft == null)
+        	throw new ParseException(text + "is not a valid feeding type", 0);
+        else
+        	return ft;
     }
     
 }
