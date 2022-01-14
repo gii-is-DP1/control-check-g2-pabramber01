f924c7b87bf0b6a1ada7f6c958b9ed392e1be8ce
unknown <pramosb01@gmail.com>
2022-01-14 08:50:27 +0100

    Ejercicio 2 hecho

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingRepository.java
@@ -3,11 +3,13 @@ package org.springframework.samples.petclinic.feeding;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface FeedingRepository extends CrudRepository<Feeding, Integer> {
     List<Feeding> findAll();
-    // List<FeedingType> findAllFeedingTypes();
+    @Query("SELECT ft FROM FeedingType ft")
+    List<FeedingType> findAllFeedingTypes();
     Optional<Feeding> findById(int id);
     Feeding save(Feeding p);
 }
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -1,5 +1,14 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +16,20 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class FeedingType {
-    Integer id;
+@Entity
+public class FeedingType extends BaseEntity {
+    
+	@NotBlank
+	@Size(min = 5, max = 30)
+	@Column(unique = true)
     String name;
+    
+	@NotBlank
     String description;
+    
+	@NotNull
+	@JoinColumn(name = "pet_type_id")
+	@ManyToOne(optional = false)
     PetType petType;
+
 }
